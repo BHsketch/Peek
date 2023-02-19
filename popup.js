a4d12299bcd00291723b82049340675108bdae79
thehthakur <thehthakur@gmail.com>
2023-02-19 13:33:21 +0530

    Just commented out some code

--- popup.js
+++ popup.js
@@ -21,12 +21,9 @@ chrome.webRequest.onCompleted.addListener(
           return;
         }
         prev_captions_url = captions_url;
-        chrome.tabs.sendMessage(
-          tabs[0].id,
-          {
-            captions: captions_url,
-          },
-        );
+        chrome.tabs.sendMessage(tabs[0].id, {
+          captions: captions_url,
+        });
       }
     );
   },
@@ -61,56 +58,56 @@ function user_input(input) {
           // let jscode = "progresscontainer = document.getElementsByClassName('ytp-progress-list')[0]";
           // jscode+="progresscontainer.innerHTML+='<div class=\"ytp-highlight-progress\" style=\"background-color:turquoise; position:absolute; height:100%; width:30px; left:\"300px\"></div>'";
 
-          // chrome.tabs.executeScript( 
-          // { 
+          // chrome.tabs.executeScript(
+          // {
           //   code:jscode
-          // }, 
-          // function() { 
-          // console.log("JavaScript executed!"); 
-          // } 
-          // ); 
+          // },
+          // function() {
+          // console.log("JavaScript executed!");
+          // }
+          // );
 
-          let durationvid = (document.getElementsByClassName('ytp-time-duration'))[0].innerHTML
-          let durationsplit = durationvid.split(':')
-          let i;
+          // let durationvid = (document.getElementsByClassName('ytp-time-duration'))[0].innerHTML
+          // let durationsplit = durationvid.split(':')
+          // let i;
 
-          for(i=0; i<durationsplit.length; i++)
-          {
-              durationsplit[i] = Number(durationsplit[i]);
-          }
+          // for(i=0; i<durationsplit.length; i++)
+          // {
+          //     durationsplit[i] = Number(durationsplit[i]);
+          // }
           //duration split just has a bunch of numbers now
 
-          i=0;
-          let j=0;
-          let durationinseconds = 0;
+          // i=0;
+          // let j=0;
+          // let durationinseconds = 0;
 
-          for(i=0; i<durationsplit.length; i++)
-          {
-          durationinseconds += (durationsplit[durationsplit.length - 1 - i])*(Math.pow(60, i));
-          }
+          // for(i=0; i<durationsplit.length; i++)
+          // {
+          // durationinseconds += (durationsplit[durationsplit.length - 1 - i])*(Math.pow(60, i));
+          // }
 
           //now we have the total duration of the video
 
-          for(i=0; i<timestamps.length; i++)
-          {
-          timestamps[i] = Math.floor(timestamps[i]);
-          let ratio = (timestamps[i])/durationinseconds;
-          ratio = ratio*100;
-          //ratio in percentage
-          //progresscontainer = document.getElementsByClassName('ytp-progress-list')[0];
-          //progresscontainer.innerHTML+='<div class=\"ytp-highlight-progress\" style=\"background-color:turquoise; position:absolute; height:100%; width:10px; left:' + toString(ratio) + '%\"></div>';
+          // for(i=0; i<timestamps.length; i++)
+          // {
+          // timestamps[i] = Math.floor(timestamps[i]);
+          // let ratio = (timestamps[i])/durationinseconds;
+          // ratio = ratio*100;
+          // //ratio in percentage
+          // //progresscontainer = document.getElementsByClassName('ytp-progress-list')[0];
+          // //progresscontainer.innerHTML+='<div class=\"ytp-highlight-progress\" style=\"background-color:turquoise; position:absolute; height:100%; width:10px; left:' + toString(ratio) + '%\"></div>';
 
-          let child = document.createElement('div');
-          child.className = 'ytp-highlight-progress';
-          child.style.backgroundColor = "turquoise";
-          child.style.position = absolute;
-          child.style.position = "absolute";
-          child.style.height = "100%";
-          child.style.left = ratio;
-          child.style.width = "10px";
-          let parent = document.getElementsByClassName('ytp-progress-list')[0];
-          parent.appendChild(child);
-          }
+          // let child = document.createElement('div');
+          // child.className = 'ytp-highlight-progress';
+          // child.style.backgroundColor = "turquoise";
+          // child.style.position = absolute;
+          // child.style.position = "absolute";
+          // child.style.height = "100%";
+          // child.style.left = ratio;
+          // child.style.width = "10px";
+          // let parent = document.getElementsByClassName('ytp-progress-list')[0];
+          // parent.appendChild(child);
+          // }
 
           while (div.childElementCount > 2) {
             div.removeChild(div.lastChild);
@@ -121,7 +118,6 @@ function user_input(input) {
             failed_message.innerHTML = "No search results found";
             div.appendChild(failed_message);
           }
-
         }
       );
     }
@@ -129,13 +125,14 @@ function user_input(input) {
 }
 function refreshButtons() {
   user_input(document.getElementById("phrase-box"));
-
 }
-document.getElementById("phrase-box").addEventListener("keyup", function (event) {
+document
+  .getElementById("phrase-box")
+  .addEventListener("keyup", function (event) {
     event.preventDefault();
     refreshButtons();
   });
-// document.getElementById('phrase-box')
+// document.getElementById('phraṭṭse-box')
 //     .addEventListener('search', function() {
 //       let k;
 //       for(k=0; k<highlights.length; k++)
