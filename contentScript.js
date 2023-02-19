60457b72b3cfb1fd103e9ecd12e2083e3f4ef324
Bhavya Hirani <bhavya25hirani@gmail.com>
2023-02-18 21:46:36 -0600

    deleted files

--- contentScript.js
+++
@@ -1,9 +0,0 @@
-console.log("content1");
-
-chrome.storage.local.get(["key"]).then((result) => {
-    console.log("Value currently is " + result.key);
-    });
-
-const stringSearched = ()=>{
-    console.log('sup');
-}
